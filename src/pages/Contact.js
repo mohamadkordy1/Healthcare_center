351b320f7711096334623a4fcfbedadc0ac4b68f
mohamad kordy <mohamadko108@gmail.com>
2026-05-30 19:52:00 +0300

    Add contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function Contact() {
+  const [state, setState] = useState({ fname: "", email: "", message: "" });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(state, null, 2));
+  };
+
+  return (
+    <section id="contact" className="section" style={{ paddingTop: "120px" }}>
+      <h2>Contact us</h2>
+
+      {/* Changed <div> to <form> but kept the 'form-box' class for styling */}
+      <form className="form-box" onSubmit={handleSubmit}>
+        <input 
+          name="fname"
+          type="text" 
+          placeholder="name"
+          value={state.fname}
+          onChange={handleChange}
+        />
+        
+        <input 
+          name="email"
+          type="email" 
+          placeholder="email"
+          value={state.email}
+          onChange={handleChange}
+        />
+        
+        <textarea 
+          name="message"
+          placeholder="message"
+          value={state.message}
+          onChange={handleChange}
+        />
+        
+        <button type="submit">send</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
